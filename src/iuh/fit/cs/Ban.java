344cc5f020de505860c8825f3f37612e897c88ec
TinhLe6806 <lecaophutinh2006@gmail.com>
2025-09-21 13:48:57 +0700

    Ban.java

--- src/iuh/fit/cs/Ban.java
+++ src/iuh/fit/cs/Ban.java
@@ -9,19 +9,38 @@ package iuh.fit.cs;
 class Ban extends CoSoVatChat {
 	private double doRong;
 	private double doDai;
-	private int soChan;
-	public double canNang() {
-		return soChan*10 + doRong*doDai*10;
-	}
-	public Ban(String ma, iuh.fit.cs.chatLieu chatLieu, iuh.fit.cs.kichCo kichCo, int soChan, double doRong,
+	public Ban(String ma, ChatLieu chatLieu, KichCo kichCo, int soChan, double doRong,
 			double doDai, double canNang) {
 		super(ma, chatLieu, kichCo, soChan);
-		this.doRong = doRong;
-		this.doDai = doDai;
-		this.canNang();
+		setDoRong(doRong);
+		setDoDai(doDai);
 	}
-	public Ban(String ma, iuh.fit.cs.chatLieu chatLieu, iuh.fit.cs.kichCo kichCo, int soChan) {
-		super(ma, chatLieu, kichCo, soChan);
+	public double getDoRong() {
+		return doRong;
+	}
+	public void setDoRong(double doRong) {
+		if(doRong <= 0) {
+			throw new IllegalArgumentException("Do rong > 0");
+		}
+		this.doRong = doRong;
+	}
+	public double getDoDai() {
+		return doDai;
+	}
+	public void setDoDai(double doDai) {
+		if(doDai <= 0) {
+			throw new IllegalArgumentException("Do dai > 0");
+		}
+		this.doDai = doDai;
+	}
+	@Override
+	public double canNang() {
+		return getSoChan()*10 + doRong*doDai*10;
+	}
+	@Override
+	public String toString() {
+		return String.format("%-8s %-10s %-10s %8s %10s",getMa(),getChatLieu(),getKichCo(),getSoChan(),getDoRong(),getDoDai(),canNang());
 	}
 	
 }
+
